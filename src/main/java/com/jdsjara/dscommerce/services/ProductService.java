ce01c5c845a656d5cb9cb86b2163fdd07929cc2d
Jorge Denys <jorgedenys@gmail.com>
2023-03-18 13:05:40 -0300

    GET product by id

---
+++ src/main/java/com/jdsjara/dscommerce/services/ProductService.java
@@ -0,0 +1,23 @@
+package com.jdsjara.dscommerce.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.jdsjara.dscommerce.dto.ProductDTO;
+import com.jdsjara.dscommerce.entities.Product;
+import com.jdsjara.dscommerce.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	@Transactional(readOnly = true)
+	public ProductDTO findById(Long id) {
+		Product product = repository.findById(id).get();
+		return new ProductDTO(product);
+	}
+	
+}
