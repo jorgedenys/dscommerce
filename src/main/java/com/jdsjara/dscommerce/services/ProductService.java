ecc6212f510cfdd7d3c5a1263b8ef8cc12545c44
Jorge Denys <jorgedenys@gmail.com>
2023-03-20 17:51:03 -0300

    Insert product

--- src/main/java/com/jdsjara/dscommerce/services/ProductService.java
+++ src/main/java/com/jdsjara/dscommerce/services/ProductService.java
@@ -1,7 +1,5 @@
 package com.jdsjara.dscommerce.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -28,6 +26,20 @@ public class ProductService {
 	public Page<ProductDTO> findAll(Pageable pageable) {
 		Page<Product> result = repository.findAll(pageable);
 		return result.map(x -> new ProductDTO(x));
+	}
+	
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {
+		
+		Product entity = new Product();
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+		
+		entity = repository.save(entity);
+		
+		return new ProductDTO(entity);
 	}
 	
 }
